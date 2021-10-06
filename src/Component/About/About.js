1d18e5378debda4c0ffa06a2f816af56aba5ed00
RafiqulIslamNiL100 <mohammadrafiqulislamnil@gmail.com>
2021-10-07 01:03:54 +0800

    ok

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -4,7 +4,7 @@ import "./About.css"
 const About = () => {
     return (
         <div>
-            <h2 className="text-center text-danger"> This is Our About Part</h2>
+            <h2 className="text-center text-success">About section</h2>
         </div>
     );
 };
