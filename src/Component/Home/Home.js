a66b3d75110d9ca3576ddb612996604d7581aec6
RafiqulIslamNiL100 <78181651+RafiqulIslamNiL100@users.noreply.github.com>
2021-10-07 14:49:58 +0800

    Update Home.js

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -4,7 +4,7 @@ import "./Home.css"
 const Home = () => {
     return (
         <div>
-            <h2 className="text-center text-info">This is a English Coaching Center</h2>
+            <h2 className="text-center text-info">Welcome to our Coaching Center</h2>
         </div>
     );
 };
